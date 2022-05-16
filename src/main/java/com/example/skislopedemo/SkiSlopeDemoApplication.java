4b8c72d0236b5c6ec1d4bc7a60e9a4cceeae9d9c
Julia Merta <julia-merta@wp.pl>
2022-05-16 17:11:27 +0200

    Files Added

---
+++ src/main/java/com/example/skislopedemo/SkiSlopeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.skislopedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiSlopeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkiSlopeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skislopedemo/SkiSlopeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skislopedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiSlopeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
